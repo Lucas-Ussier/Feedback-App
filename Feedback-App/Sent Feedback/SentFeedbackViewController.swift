9c5d74136680b2298cefbe969d55e99bcb6ba6d2
Lucas <lukedbzgalvao@gmail.com>
2022-10-27 14:44:51 -0300

    New File Added

---
+++ Feedback-App/Sent Feedback/SentFeedbackViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SentFeedbackViewController.swift
+//  Feedback-App
+//
+//  Created by Lucas Galvao on 27/10/22.
+//
+
+import UIKit
+
+class SentFeedbackViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+    }
+}
